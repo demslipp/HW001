16b46ceb65e4608a8b8ea27b5d46a542d9238e78
Test1 <jk1998@yandex.ru>
2020-03-10 18:40:39 +0300

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Main{
     public static void main (String[] args){
 
@@ -13,8 +15,9 @@ public class Main{
         System.out.println (employee.getName ()+", "+employee.getAnnualSalary ()+", "+employee.raiseSalary (30));
         System.out.println (employee.toString ());
 
-        Book book = new Book ();
-        System.out.println ();
+        Book book = new Book ("asd", 0, 10.1);
+        //System.out.println ();
+
 
     }
 }
--- src/MyPoint.java
+++ src/MyPoint.java
@@ -1,2 +1,28 @@
-public class MyPoint{
-}
+/*public class MyPoint{
+    private int x =0;
+    private int y =0;
+
+    public MyPoint(){
+
+    }
+    public MyPoint(int x, int y){}
+
+    public int getX (){
+        return x;
+    }
+
+    public void setX (int x){
+        this.x=x;
+    }
+
+    public int getY (){
+        return y;
+    }
+
+    public void setY (int y){
+        this.y=y;
+    }
+    public int[2] getXY(){
+
+    }
+}*/
